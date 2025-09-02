dafe200207c25837ea4c14e6ce18ce49854ee374
TronnoxUwU <baptisterichard98@free.fr>
2025-09-02 18:23:23 +0200

    page about & contact

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Hello Django!</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Contact us</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About us</h1>")
