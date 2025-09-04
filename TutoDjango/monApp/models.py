739720007b2721db8a9d02b915f233a63ce5f671
TronnoxUwU <baptisterichard98@free.fr>
2025-09-04 09:26:46 +0200

    ajout des models & migrations faites

--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    idCat = models.AutoField(primary_key=True)
+    nomCat = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nomCat
+
+class Produit(models.Model):
+    refProd = models.AutoField(primary_key=True)
+    intituleProd = models.CharField(max_length=200)
+    prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
+    dateFabProd = models.DateField(null=True, blank=True)
+    # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie 1,1 côté produit)→
+    categorie = models.ForeignKey(Categorie,
+                                  on_delete=models.CASCADE,
+                                  related_name="produits",
+                                  null=True,
+                                  blank=True)
+
+    def __str__(self):
+        return self.intituleProd
+
+class Rayon(models.Model):
+    idRayon = models.AutoField(primary_key=True)
+    nomRayon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nomRayon
+
+class Contenir(models.Model):
+    pk = models.CompositePrimaryKey("refProd", "idRayon")
+    refProd = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    idRayon = models.ForeignKey(Rayon, on_delete=models.CASCADE)
+    qte = models.IntegerField()
+
+class Statut(models.Model):
+    
