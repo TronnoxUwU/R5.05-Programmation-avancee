24775aec125f59c5adf3587a98718e1978b665f6
TronnoxUwU <baptisterichard98@free.fr>
2025-09-11 09:31:00 +0200

    fin css

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path("home/<param>", views.home, name="home"),
-    path("home", views.home, name="home"),
-    path("contact", views.contact, name="contact"),
-    path("about", views.about, name="about"),
-    path("produits", views.produits, name="produits"),
+    path("home/", views.home, name="home"),
+    path("contact/", views.contact, name="contact"),
+    path("about/", views.about, name="about"),
+    path("produits/", views.ListProduits, name="produits"),
+    path('categories/',views.ListCategories ,name='categories'),
+    path('statuts/',views.ListStatuts,name='statuts'),
+    path('rayons/',views.ListRayons,name='rayons'),
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,19 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from monApp.models import *
 
 def home(request, param=""):
+    if request.GET and request.GET.get("test"):
+        raise Http404
     return HttpResponse(f"<h1>Hello {param}</h1>")
 
 def contact(request):
-    return HttpResponse("<h1>Contact us</h1>")
+    return render(request, 'monApp/contact.html')
 
 def about(request):
-    return HttpResponse("<h1>About us</h1>")
+    return render(request, 'monApp/about.html')
 
-def produits(request):
+# def produits(request):
+#     prdts = Produit.objects.all()
+#     txt = ""
+#     for p in prdts:
+#         txt += f"<li>{p.intituleProd}</li> "
+#     return HttpResponse(f"<ul>{txt}</ul>")
+
+def accueil(request,param):
+    return HttpResponse(f"<h1>Hello {param}! You're connected</h1>")
+
+def ma_vue(request):
+    return JsonResponse({'foo': 'bar'})
+
+def ListProduits(request):
     prdts = Produit.objects.all()
-    txt = ""
-    for p in prdts:
-        txt += f"<li>{p.intituleProd}</li> "
-    return HttpResponse(f"<ul>{txt}</ul>")
+    return render(request, 'monApp/list_produits.html', {'prdts': prdts})
+
+def ListCategories(request):
+    cats = Categorie.objects.all()
+    return render(request, 'monApp/list_categories.html', {'cats': cats})
+
+def ListStatuts(request):
+    stats = Statut.objects.all()
+    return render(request, 'monApp/list_statuts.html', {'stats': stats})
+
+def ListRayons(request):
+    rays = Rayon.objects.all()
+    return render(request, 'monApp/list_rayons.html', {'rays': rays})
