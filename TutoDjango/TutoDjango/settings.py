8486fb125cebba03f5f8b3ebb4f1a96d60ba0ddf
TronnoxUwU <baptisterichard98@free.fr>
2025-09-11 08:16:30 +0200

    fin TD2 debut TP2

--- TutoDjango/TutoDjango/settings.py
+++ TutoDjango/TutoDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'monApp',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'TutoDjango.urls'
@@ -128,3 +130,7 @@ STATICFILES_DIRS = (BASE_DIR/'static/',)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media/'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
