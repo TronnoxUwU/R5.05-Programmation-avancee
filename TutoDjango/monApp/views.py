84c3feea56bdfbc1c3a1d65136847bede3b0cc2d
TronnoxUwU <baptisterichard98@free.fr>
2025-09-18 09:23:08 +0200

    TD3 page 13

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from monApp.models import *
 from django.views.generic import *
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
+from forms import *
 
 class HomeView(TemplateView):
     template_name = "monApp/page_home.html"
@@ -20,31 +23,124 @@ class AboutView(TemplateView):
         context = super(AboutView, self).get_context_data(**kwargs)
         context['titreh1'] = "About us..."
         return context
-    
+
     def post(self, request, **kwargs):
         return render(request, self.template_name)
 
-class ContactView(TemplateView):
-    template_name = "monApp/page_home.html"
+# class ContactView(TemplateView):
+#     template_name = "monApp/page_home.html"
+#     def get_context_data(self, **kwargs):
+#         context = super(ContactView, self).get_context_data(**kwargs)
+#         context['titreh1'] = "Contact us..."
+#         return context
+
+#     def post(self, request, **kwargs):
+#         return render(request, self.template_name)
+    
+def ContactView(request):
+    form = ContactUsForm()
+    titreh1 = "Contact us !"
+    return render(request, "monApp/page_home.html",{'titreh1':titreh1, 'form':form})
+    
+class ProduitListView(ListView):
+    model = Produit
+    template_name = "monApp/list_produits.html"
+    context_object_name = "prdts"
+    # queryset = Produit.objects.filter(refProd=2)
+
+    def get_queryset(self ) :
+        return Produit.objects.order_by("prixUnitaireProd")
+
     def get_context_data(self, **kwargs):
-        context = super(ContactView, self).get_context_data(**kwargs)
-        context['titreh1'] = "Contact us..."
+        context = super(ProduitListView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Liste de mes produits"
         return context
     
+class StatutListView(ListView):
+    model = Statut
+    template_name = "monApp/list_statuts.html"
+    context_object_name = "stats"
+
+    
+class CategorietListView(ListView):
+    model = Categorie
+    template_name = "monApp/list_categories.html"
+    context_object_name = "cats"
+    
+class RayonListView(ListView):
+    model = Rayon
+    template_name = "monApp/list_rayons.html"
+    context_object_name = "rays"
+
+
+class ProduitDetailView(DetailView):
+    model = Produit
+    template_name = "monApp/detail_produit.html"
+    context_object_name = "prdt"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProduitDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du produit"
+        return context
+    
+class CategorieDetailView(DetailView):
+    model = Categorie
+    template_name = "monApp/detail_categorie.html"
+    context_object_name = "cat"
+    
+class StatutDetailView(DetailView):
+    model = Statut
+    template_name = "monApp/detail_statut.html"
+    context_object_name = "stat"
+    
+class RayonDetailView(DetailView):
+    model = Rayon
+    template_name = "monApp/detail_rayon.html"
+    context_object_name = "ray"
+
+class ConnectView(LoginView):
+    template_name = 'monApp/page_login.html'
+
     def post(self, request, **kwargs):
+        lgn = request.POST.get('username', False)
+        pswrd = request.POST.get('password', False)
+        user = authenticate(username=lgn, password=pswrd)
+        if user is not None and user.is_active:
+            login(request, user)
+            return render(request, 'monApp/page_home.html', {'param': lgn, 'message': "You're connected"})
+        else:
+            return render(request, 'monApp/page_register.html')
+        
+class RegisterView(TemplateView):
+    template_name = 'monApp/page_register.html'
+    def post(self, request, **kwargs):
+        username = request.POST.get('username', False)
+        mail = request.POST.get('mail', False)
+        password = request.POST.get('password', False)
+        user = User.objects.create_user(username, mail, password)
+        user.save()
+        if user is not None and user.is_active:
+            return render(request, 'monApp/page_login.html')
+        else:
+            return render(request, 'monApp/page_register.html')
+  
+class DisconnectView(TemplateView):
+    template_name = 'monApp/page_logout.html'
+
+    def get(self, request, **kwargs):
+        logout(request)
         return render(request, self.template_name)
 
+# def home(request, param=""):
+#     if request.GET and request.GET.get("test"):
+#         raise Http404
+#     return HttpResponse(f"<h1>Hello {param}</h1>")
 
-def home(request, param=""):
-    if request.GET and request.GET.get("test"):
-        raise Http404
-    return HttpResponse(f"<h1>Hello {param}</h1>")
+# def contact(request):
+#     return render(request, 'monAppstats/contact.html')
 
-def contact(request):
-    return render(request, 'monApp/contact.html')
-
-def about(request):
-    return render(request, 'monApp/about.html')
+# def about(request):
+#     return render(request, 'monApp/about.html')
 
 # def produits(request):
 #     prdts = Produit.objects.all()
@@ -53,24 +149,24 @@ def about(request):
 #         txt += f"<li>{p.intituleProd}</li> "
 #     return HttpResponse(f"<ul>{txt}</ul>")
 
-def accueil(request,param):
-    return HttpResponse(f"<h1>Hello {param}! You're connected</h1>")
+# def accueil(request,param):
+#     return HttpResponse(f"<h1>Hello {param}! You're connected</h1>")
 
-def ma_vue(request):
-    return JsonResponse({'foo': 'bar'})
+# def ma_vue(request):
+#     return JsonResponse({'fur': 'bar'})
 
-def ListProduits(request):
-    prdts = Produit.objects.all()
-    return render(request, 'monApp/list_produits.html', {'prdts': prdts})
+# def ListProduits(request):
+#     prdts = Produit.objects.all()
+#     return render(request, 'monApp/list_produits.html', {'prdts': prdts})
 
-def ListCategories(request):
-    cats = Categorie.objects.all()
-    return render(request, 'monApp/list_categories.html', {'cats': cats})
+# def ListCategories(request):
+#     cats = Categorie.objects.all()
+#     return render(request, 'monApp/list_categories.html', {'cats': cats})
 
-def ListStatuts(request):
-    stats = Statut.objects.all()
-    return render(request, 'monApp/list_statuts.html', {'stats': stats})
+# def ListStatuts(request):
+#     stats = Statut.objects.all()
+#     return render(request, 'monApp/list_statuts.html', {'stats': stats})
 
-def ListRayons(request):
-    rays = Rayon.objects.all()
-    return render(request, 'monApp/list_rayons.html', {'rays': rays})
+# def ListRayons(request):
+#     rays = Rayon.objects.all()
+#     return render(request, 'monApp/list_rayons.html', {'rays': rays})
